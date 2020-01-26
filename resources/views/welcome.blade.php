d5a9c7ff5665f362e293f8a9b7146d02912ebf9f
EC2 Default User <ec2-user@ip-172-31-12-254.us-east-2.compute.internal>
2020-01-26 07:48:41 +0000

    top page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,6 @@
     <div class="center jumbotron">
         <div class="text-center">
             <h1>Welcome to the Microposts</h1>
-            {!! link_to_route('signup.get', 'sign up now!', null, ['class' => 'btn btn-lg btn-primary']) !!}
         </div>
     </div>
 @endsection
